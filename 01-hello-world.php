1ef7de999d35fad6918def7800f445add094e4a0
kewen <kauakewen1010@hotmail.com>
2024-05-14 17:25:31 -0300

    Create 01-hello-world.php

---
+++ 01-hello-world.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula 01 de PHP</title>
+</head>
+<body>
+    <h1>Experiências com PHP</h1>
+    <hr>
+
+    <!-- Delimitadores de abertura/fechamento PHP -->
+    <?php
+    echo "Olá!";
+    echo "<p>Senac Penha</p>";
+    ?>  
+    <!-- Delimitadores de abertura/fechamento PHP -->
+
+</body>
+</html>
