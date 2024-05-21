3b6c9ff61e95118386033a82361c84fd520c8be9
kewen <kauakewen1010@hotmail.com>
2024-05-21 15:22:40 -0300

    Update exercicio03.php

--- exercicio03.php
+++ exercicio03.php
@@ -5,7 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <style>
             li:nth-child(odd) {
+                border: solid;
                 background-color: gray;
+            }
+
+            li:nth-child(even) {
+                border: solid;
             }
     </style>
     <title>Exercicio 3</title>
