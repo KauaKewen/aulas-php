f1717605c8b1628f75228eeaf5f1324cca330a13
kewen <kauakewen1010@hotmail.com>
2024-05-21 15:22:46 -0300

    Create index.php

---
+++ site/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Site</title>
+</head>
+<body>
+    <header>
+        <h1>Site Exemplo</h1>
+        <nav>
+            <a href="index.php">Home</a>
+            <a href="equipe.php">Equipe</a>
+            <a href="portfolio.php">Portfólio</a>
+        </nav>
+    </header>
+    <main>
+        <article>
+            <h2>Bem-vindo(a)</h2>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere, temporibus id. Deleniti quae nemo quia fuga, possimus, mollitia tenetur temporibus officiis ab repudiandae molestiae laborum similique adipisci nihil dolor. Facilis!</p>
+        </article>
+    </main>
+    <footer>
+        <h2>Site criado como exemplo</h2>
+    </footer>
+</body>
+</html>
