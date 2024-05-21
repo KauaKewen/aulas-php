beebb049325b17063464d61e09ca3285f7cbd9b7
kewen <kauakewen1010@hotmail.com>
2024-05-21 16:22:55 -0300

    Update index.php

--- site/index.php
+++ site/index.php
@@ -1,27 +1,7 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Site</title>
-</head>
-<body>
-    <header>
-        <h1>Site Exemplo</h1>
-        <nav>
-            <a href="index.php">Home</a>
-            <a href="equipe.php">Equipe</a>
-            <a href="portfolio.php">Portfólio</a>
-        </nav>
-    </header>
-    <main>
-        <article>
+
+<?php include "cabecalho.php"; ?>
+
             <h2>Bem-vindo(a)</h2>
             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere, temporibus id. Deleniti quae nemo quia fuga, possimus, mollitia tenetur temporibus officiis ab repudiandae molestiae laborum similique adipisci nihil dolor. Facilis!</p>
-        </article>
-    </main>
-    <footer>
-        <h2>Site criado como exemplo</h2>
-    </footer>
-</body>
-</html>
+     
+<?php include "rodape.php"; ?>
