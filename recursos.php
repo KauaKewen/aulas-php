353752a5882ea93f13c3c85362f727cc654a4a48
kewen <kauakewen1010@hotmail.com>
2024-05-21 15:22:42 -0300

    Create recursos.php

---
+++ recursos.php
@@ -0,0 +1,7 @@
+<?php
+
+const EMPRESA = "ABC Tecnologia";
+$anoFiscal = 2024;
+$tecnologias = ["Phyton", "IA Generativa", "Machine Learning"];
+
+/* Obs.: em arquivos que possuem somente código PHP, não é necessário fazer o fechamento "?>" */
