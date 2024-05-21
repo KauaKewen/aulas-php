4bf883a833aeaf088e81bdf49ae79368f4ea0cff
kewen <kauakewen1010@hotmail.com>
2024-05-21 15:22:36 -0300

    Update 01-hello-world.php

--- 01-hello-world.php
+++ 01-hello-world.php
@@ -1,9 +1,11 @@
+<?php include "recursos.php"; ?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Aula 01 de PHP</title>
+    <title><?=EMPRESA?> Aula 01 de PHP</title>
 </head>
 <body>
     <h1>Experiências com PHP</h1>
@@ -16,5 +18,11 @@
     ?>  
     <!-- Delimitadores de abertura/fechamento PHP -->
 
+
+    <hr>
+    <h2>Tecnologias trabalhadas</h2>
+    <?php foreach($tecnologias as $tecnologia){ ?>
+        <p><?=$tecnologia?></p>
+ <?php } ?>
 </body>
 </html>
