779b422685cd82763ae63543b7c69dc346a2dae9
kewen <kauakewen1010@hotmail.com>
2024-05-21 17:00:50 -0300

    Update index.php

--- site/index.php
+++ site/index.php
@@ -1,5 +1,7 @@
 
-<?php include "cabecalho.php"; ?>
+<?php
+$title = "Home - Sites";
+ include "cabecalho.php"; ?>
 
             <h2>Bem-vindo(a)</h2>
             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere, temporibus id. Deleniti quae nemo quia fuga, possimus, mollitia tenetur temporibus officiis ab repudiandae molestiae laborum similique adipisci nihil dolor. Facilis!</p>
