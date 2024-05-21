b52a30037f3f79071508f5291d12585d7e1b59be
kewen <kauakewen1010@hotmail.com>
2024-05-21 15:22:51 -0300

    Create portfolio.php

---
+++ site/portfolio.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfolio - Site</title>
+</head>
+<body>
+    <header>
+        <h1>Site Exemplo</h1>
+        <nav>
+            <a href="index.php">Home</a>
+            <a href="equipe.php">Equipe</a>
+            <a href="portfolio.php">Portfólio</a>
+        </nav>
+    </header>
+    <main>
+        <article>
+            <h2>Nossos trabalhos</h2>
+            <details>
+                <summary>Game</summary>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, nemo.</p>
+            </details>
+            <details>
+                <summary>Web Site</summary>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo dolores laborum nesciunt ad esse tempora vel ex dolore recusandae ipsa!</p>
+            </details>
+        </article>
+    </main>
+    <footer>
+        <h2>Site criado como exemplo</h2>
+    </footer>
+</body>
+</html>
