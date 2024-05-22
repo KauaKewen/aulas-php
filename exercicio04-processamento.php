6f2a7fe81ae56b246c6e9029a8791a82b73d7573
kewen <kauakewen1010@hotmail.com>
2024-05-22 17:10:53 -0300

    Update exercicio04-processamento.php

--- exercicio04-processamento.php
+++ exercicio04-processamento.php
@@ -13,6 +13,20 @@
 necessária para obter os dados vindos do formulário
 e mostrá-los usando elementos HTML (da forma que você quiser). -->
 
+<?php
+$produto = $_POST['produto'];
+$fabricante = $_POST['fabricante'];
+$preco = $_POST['preco'];
+$descricao = $_POST['descricao'];
+?>
+
+<ul>
+    <li><?=$produto?></li>
+    <li><?=$fabricante?></li>
+    <li>R$<?=number_format($preco, 2, ',' , '.')?></li>
+    <li><?=$descricao?></li>
+</ul>
+
     
 </body>
 </html>
